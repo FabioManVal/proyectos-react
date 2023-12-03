99e799cef862359f7d591c842a665bc1200813bd
Fabio Mantilla <25933169+FabioManVal@users.noreply.github.com>
2023-12-03 16:36:35 -0500

    fallo al tomar número aleatorio y sin estilos.

--- 01-adivina-numero/src/pages/App.jsx
+++ 01-adivina-numero/src/pages/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react';
-import reactLogo from './../assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useState } from "react";
+import { OffCanvas } from "../components/OffCanvas";
+import reactLogo from "./../assets/svg/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
+  const [randomNumber, setRandomNumber] = useState("");
+
+  const addRandomNumber = (number) => {
+    console.log(number);
+    setRandomNumber(number);
+  };
 
   return (
     <>
+      <OffCanvas returnRandomNumber={addRandomNumber}></OffCanvas>
+      <h1>{randomNumber}</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
